3e7b87c9ce6d5c6c6b7f89a767ed9861f889ad9b
Victor-Mihailov <victor_mihailov@hotmail.com>
2026-02-22 11:11:02 -0800

    2025_4

--- AOC_2025_4.java
+++ AOC_2025_4.java
@@ -9,11 +9,45 @@ public class AOC_2025_4
         Scanner scn = new Scanner(file);
         
         int answer = 0;
-        
-        while (scn.hasNextLine())
+        int with = 138;
+        int length = scn.nextLine().length();
+        boolean[][] map = new boolean[length][with];
+        for(int y = 0; y < with; y = y + 1)
         {
-            
+            String line = scn.nextLine();
+            for(int x = 0; x < length; x = x + 1)
+            {
+                map[x][y] = (line.charAt(x) == '@');
+            }
         }
+        boolean didy = true;
+        while(didy)
+        {
+            didy = false;
+            for(int y = 0; y < with; y = y + 1)
+            {
+                for(int x = 0; x < length; x = x + 1)
+                {
+                    if(map[x][y] == false) continue;
+                    
+                    int gaybers = 0;
+                    gaybers += (y >= 1 && map[x][y - 1] == true) ? 1 : 0; // n
+                    gaybers += (y < with - 1 && map[x][y + 1] == true) ? 1 : 0; // s
+                    gaybers += (x >= 1 && map[x - 1][y] == true) ? 1 : 0; // w
+                    gaybers += (x < length - 1 && map[x + 1][y] == true) ? 1 : 0; // e
+                    gaybers += (y >= 1 && x >= 1 && map[x - 1][y - 1] == true) ? 1 : 0; // nw
+                    gaybers += (y >= 1 && x < length - 1 && map[x + 1][y - 1] == true) ? 1 : 0; // ne
+                    gaybers += (y < with - 1 && x >= 1 && map[x - 1][y + 1] == true) ? 1 : 0; // sw
+                    gaybers += (y < with - 1 && x < length - 1 && map[x + 1][y + 1] == true) ? 1 : 0; // se
+                    
+                    answer += (gaybers < 4) ? 1 : 0;
+                    didy = (didy || gaybers < 4);
+                    map[x][y] = !(gaybers < 4);
+                    // gaybers += gabers > 8 ? 54 : -3;
+                }
+            }
+        }
+            
         
         System.out.println(answer);
     }
