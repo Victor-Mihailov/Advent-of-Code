2dd619e6cea02b450e48174cd6097af8beba997e
Victor-Mihailov <victor_mihailov@hotmail.com>
2026-03-05 07:02:25 -0800

    Fin 25 7

--- AOC_2025_7.java
+++ AOC_2025_7.java
@@ -8,10 +8,10 @@ public class AOC_2025_7
         File file = new File("input.txt");
         Scanner scn = new Scanner(file);
         
-        int answer = 0;
+        long answer = 0;
         int length = scn.nextLine().length();
         int width = 142;
-        int[][] map = new int[length][width];
+        long[][] map = new long[length][width];
         
         
         for(int y = 0; y < width; y = y + 1)
@@ -20,8 +20,8 @@ public class AOC_2025_7
             //System.out.println();
             for(int x = 0; x < length; x = x + 1)
             {
-                map[x][y] = (line.charAt(x) == '^') ? 1 : map[x][y];
-                map[x][y] = (line.charAt(x) == 'S') ? 2 : map[x][y];
+                map[x][y] = (line.charAt(x) == '^') ? -1 : map[x][y];
+                map[x][y] = (line.charAt(x) == 'S') ? 1 : map[x][y];
                 //System.out.print(map[x][y]);
             }
         }
@@ -32,31 +32,53 @@ public class AOC_2025_7
             for(int x = 0; x < length; x = x + 1)
             {
                 System.out.print(map[x][y]);
-                if (map[x][y] >= 2)
+                if (map[x][y] >= 1)
                 {
-                    if(map[x][y + 1] == 1) // broken?
+                    if(map[x][y + 1] == -1) // broken?
                     {
-                        map[x - 1][y + 1] = (map[x - 1][y + 1] == 0) ? 2 : map[x - 1][y + 1] + map[x][y] - 1;
-                        map[x + 1][y + 1] = (map[x + 1][y + 1] == 0) ? 2 : map[x + 1][y + 1] + map[x][y] - 1;
+                        map[x - 1][y + 1] = map[x - 1][y + 1] + map[x][y];
+                        map[x + 1][y + 1] = map[x + 1][y + 1] + map[x][y];
                     }
-                    else
+                    else if(map[x][y + 1] >= 0)
                     {
-                        map[x][y + 1] = (map[x][y + 1] == 0) ? 2 : map[x][y + 1] + map[x][y] - 1;
+                        map[x][y + 1] = map[x][y + 1] + map[x][y];
                     }
                 }
                 
             }
         }
+        
+        for(int x = 0; x < length; x = x + 1)
+        {
+            answer = answer + map[x][width - 1];
+        }
+        
         // code below is for bug fixxing
+        
         for(int y = 0; y < width; y = y + 1)
         {
             System.out.println();
             for(int x = 0; x < length; x = x + 1)
             {
-                System.out.print(map[x][y]);
+                if(map[x][y] == -1)
+                {
+                    System.out.print("^");
+                }
+                else if(map[x][y] == 0)
+                {
+                    System.out.print(".");
+                }
+                else if(map[x][y] < 10)
+                {
+                    System.out.print(map[x][y]);
+                }
+                else
+                {
+                    System.out.print("|");
+                }
             }
         }
-        
+        System.out.println();
         System.out.println(answer);
     }
 }
