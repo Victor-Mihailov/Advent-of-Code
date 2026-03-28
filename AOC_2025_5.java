b6e3d83d63ab1036e39cfe5c826ab8929f05c6ee
Victor-Mihailov <victor_mihailov@hotmail.com>
2026-03-28 14:32:13 -0700

    25 5 1

--- AOC_2025_5.java
+++ AOC_2025_5.java
@@ -8,28 +8,46 @@ public class AOC_2025_5
         File file = new File("input.txt");
         Scanner scn = new Scanner(file);
         
-        int answer = 0;
+        long answer = 0;
         final int rn = 172;
         ArrayList<String> stuff = new ArrayList<String>();
         for(int l = 0; l < rn; l = l + 1)
         {
-            String line = scn.nextLine();
-            
-            String[] newParts = line.split("-");
-            
-            for(int i = 0; i < stuff.size(); i = i + 1)
+            stuff.add(scn.nextLine());
+        } 
+        boolean didy = true;
+        while(didy)
+        {
+            didy = false;
+            for(int i = 1; i < stuff.size(); i = i + 1)
             {
-                String[] oldParts = stuff.get(i).split("-");
-                if(Integer.parseInt(newParts[0]) <= Integer.parseInt(oldParts[0]) 
-                && Integer.parseInt(oldParts[0]) <= Integer.parseInt(newParts[1])
-                && Integer.parseInt(newParts[1]) <= Integer.parseInt(oldParts[1]))
-                {
-                    
-                }
+                long min = Math.min(Long.parseLong(stuff.get(i - 1).split("-")[0]), Long.parseLong(stuff.get(i).split("-")[0]));
+                long max = Math.max(Long.parseLong(stuff.get(i - 1).split("-")[1]), Long.parseLong(stuff.get(i).split("-")[1]));
             }
-            
         }
         
+        
+        /*
+        scn.nextLine();
+        while(scn.hasNextLine())
+        {
+            long n = Long.parseLong(scn.nextLine());
+            boolean flag = false;
+            for(int i = 0; i < rn; i = i + 1)
+            {
+                if(n >= Long.parseLong(stuff.get(i).split("-")[0]) && n <= Long.parseLong(stuff.get(i).split("-")[1]))
+                {
+                    flag = true;
+                }
+            }
+            if(flag)
+            {
+                answer = answer + 1;
+            }
+        }
+        */
         System.out.println(answer);
     }
+    
+    
 }
