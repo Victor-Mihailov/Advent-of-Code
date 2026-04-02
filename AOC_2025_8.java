44848fdad0eee2cdb458d0d01d0f1238679add6b
Victor-Mihailov <victor_mihailov@hotmail.com>
2026-04-02 00:54:18 -0500

    work

--- AOC_2025_8.java
+++ AOC_2025_8.java
@@ -19,16 +19,40 @@ public class AOC_2025_8
             {
                 point[i] = Integer.parseInt(temp[i]);
             }
+            pointList.add(point);
         }
         
         ArrayList<int[]> lineList = new ArrayList<int[]>();
-        for(int i = 0; i < NOP; i = i + 1)
+        for(int t = 0; t < 1000; t = t + 1)
         {
-            for(int j = 0; j < NOP - i; j = j + 1)
+            int smallest = Integer.MAX_VALUE;
+            int[] choosen = new int[2];
+            for(int i = 0; i < NOP; i = i + 1)
             {
-                
+                for(int j = 1; j < NOP - i; j = j + 1)
+                {
+                    int fx = pointList.get(i)[0];
+                    int sx = pointList.get(i + j)[0];
+                    int fy = pointList.get(i)[1];
+                    int sy = pointList.get(i + j)[1];
+                    int fz = pointList.get(i)[2];
+                    int sz = pointList.get(i + j)[2];
+                    int lx = (fx - sx) * (fx - sx);
+                    int ly = (fy - sy) * (fy - sy);
+                    int lz = (fz - sz) * (fz - sz);
+                    int line = lx + ly + lz;
+                    int[] pair = new int[2];
+                    pair[0] = i;
+                    pair[1] = i + j;
+                    smallest = (line < smallest && lineList.contains(pair) == false) ? line : smallest;
+                    
+                    choosen = (line < smallest && lineList.contains(pair) == false) ? pair : choosen;
+                }
             }
+            lineList.add(0, choosen);
+            System.out.println(choosen[1]);
         }
+        
         
         
         
