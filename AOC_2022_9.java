c91fca72f8fad94538ffe04223dbbcdb7ff49e2f
Victor-Mihailov <victor_mihailov@hotmail.com>
2026-07-16 16:01:00 -0500

    Commented stuff

--- AOC_2022_9.java
+++ AOC_2022_9.java
@@ -1,13 +1,17 @@
-import java.util.*;
-import java.io.*;
+// Note to futer self. It seems the fundemental way I am doing the proble is wrong. I need to simulate the whole rope at once.
 
+
+
+import java.util.*;
+import java.io.*; //                                               |||||| 
+ // I am going to comment my code cause I have no clue what to do. vvvvvv
 public class AOC_2022_9
 {
     public static void main(String[] args) throws IOException
     {
         File file = new File("input.txt");
-        Scanner scn = new Scanner(file);
-         // I am going to comment my code cause I have no clue what to do.
+        Scanner scn = new Scanner(file); 
+        
         int answer = 0; // Answer is the thing that will be printed. It is supposed to be the number of unique tiles the tail has visited.
         ArrayList<String> instructions = new ArrayList<String>(); // The instructions on where the head should go.
         while(scn.hasNextLine()) // While loop to go over the imput. 
@@ -19,28 +23,28 @@ public class AOC_2022_9
          {
             answer = 1; // This is to take into acount that the starting position counts to the total positions reached.
             ArrayList<String> path = new ArrayList<String>(); // 'Path' is the marking of the positions reached.
-            int xF = 0; // xF is the 
-            int xB = 0;
-            int yF = 0;
-            int yB = 0;
-            String tale = xB + " " + yB;
-            path.add(tale);
-            ArrayList<String> directions = new ArrayList<String>();
-            display(xF, yF, xB, yB, b);
-            for(int c = 0; c < instructions.size(); c = c + 1)
+            int xF = 0; // xF is the x posision of the leading segment.
+            int xB = 0; // xB is the x posision of the following segment.
+            int yF = 0; // yF is the y posisoin of the leading segmant.
+            int yB = 0; // yB is the y posisoin of the following segmant.
+            String tale = xB + " " + yB; // By combining the x and y posisions of the following segments the tale is made.
+            path.add(tale); // This adds 0,0 to the locations visited.
+            ArrayList<String> directions = new ArrayList<String>(); // Is a temp way to store the the instructions for the next segment.
+            //display(xF, yF, xB, yB, b);
+            for(int c = 0; c < instructions.size(); c = c + 1) // A for loop to go over the instructions. Note - Can be made into a while loop.
             {
                 //display(xF, yF, xB, yB, b);
                 
                 //System.out.println(xB);
-                Scanner scan = new Scanner(instructions.get(c));
-                String scaned = scan.next();
-                if(scaned.equals("R"))
+                Scanner scan = new Scanner(instructions.get(c)); // A new scanner to go over a line of instructions.
+                String scaned = scan.next();  // Scanning the first part of the line to tell which derection.
+                if(scaned.equals("R")) // Cheaks if the direction of travel is right.
                 {
                     // System.out.println(xB);
-                    scaned = scan.next();
-                    for(int m = 0; m < Integer.parseInt(scaned); m = m + 1)
+                    scaned = scan.next(); // Scans to see the distents travaled right.
+                    for(int m = 0; m < Integer.parseInt(scaned); m = m + 1) // Goes over moving one position at a time, based on the total positions needed to be moved. Note - This whole thing could probely be done in a better way.
                     {
-                        xF = xF + 1;
+                        xF = xF + 1; //  
                         if(xB + 1 < xF)
                         {
                             xB = xB + 1;
@@ -58,7 +62,7 @@ public class AOC_2022_9
                             if(path.contains(tale) == false)
                             {
                                 answer = answer + 1;
-                                path.add(tale); // Made answer go up
+                                path.add(tale); //
                             }
                         }
                     }
@@ -87,7 +91,7 @@ public class AOC_2022_9
                             if(path.contains(tale) == false)
                             {
                                 answer = answer + 1;
-                                path.add(tale);// Made answer go up
+                                path.add(tale);// 
                             }
                         }
                     }
@@ -115,7 +119,7 @@ public class AOC_2022_9
                             if(path.contains(tale) == false)
                             {
                                 answer = answer + 1;
-                                path.add(tale); // Made answer go up
+                                path.add(tale); // 
                             }
                         }
                     }
@@ -143,7 +147,7 @@ public class AOC_2022_9
                             if(path.contains(tale) == false)
                             {
                                 answer = answer + 1;
-                                path.add(tale); // Made answer go up
+                                path.add(tale); // 
                             }
                         }
                     }
